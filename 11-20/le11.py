70c3c79c41b2c51dd9ace56cb1652a520ea1255d
andre <andre.chow@outlook.com>
2025-01-26 17:15:20 +0900

    1

--- 11-20/le11.py
+++ 11-20/le11.py
@@ -15,15 +15,28 @@ load_dotenv()  # 读取.env文件
 model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.8)
 
 subjects_prompt = """生成 5 个与 {topic} 相关的关键字，并用逗号分隔。"""
+joke_prompt = """编一个关于{subject}的中文笑话"""
 
 # 笑话主题
 class Subjects(BaseModel):
     subjects: list[str]
 
+# 笑话
+class Joke(BaseModel):
+    joke: str
+
 # 主图的整体状态
 class OverallState(TypedDict):
+    # 主题
     topic: str
+    # 主题列表
     subjects: list[str]
+    # 笑话列表
+    jokes: Annotated[list, operator.add]
+
+# 笑话节点状态（生成一个笑话）
+class JokeState(TypedDict):
+    subject: str
 
 ####################################################################################################
 ## Nodes
@@ -33,13 +46,29 @@ def generate_topics(state: OverallState):
     response = model.with_structured_output(Subjects).invoke(prompt)
     return {"subjects": response.subjects}
 
+# 这是我们根据主题生成笑话的地方
+def generate_joke(state: JokeState):
+    prompt = joke_prompt.format(subject=state["subject"])
+    response = model.with_structured_output(Joke).invoke(prompt)
+    return {"jokes": [response.joke]}
+
+####################################################################################################
+## Edges
+
+# 定义遍历生成的主题的逻辑(条件边)
+def continue_to_jokes(state: OverallState):
+    # 遍历所有主题，导向到生成笑话的节点
+    return [Send("generate_joke", {"subject": s}) for s in state["subjects"]]
+
 ####################################################################################################
 ## Graph
 # 构建图：这里我们将所有内容放在一起构建我们的图
 graph = StateGraph(OverallState)
 graph.add_node("generate_topics", generate_topics)
+graph.add_node("generate_joke", generate_joke)
 graph.add_edge(START, "generate_topics")
-graph.add_edge("generate_topics", END)
+graph.add_conditional_edges("generate_topics", continue_to_jokes, ["generate_joke"])
+graph.add_edge("generate_joke", END)
 
 app = graph.compile()
 app.get_graph().draw_mermaid_png(output_file_path="graph.png")
@@ -50,4 +79,3 @@ result = app.invoke({"topic": topic_title})
 pprint.pprint(result)
 
 print(evalEndTime(start_time))
-
